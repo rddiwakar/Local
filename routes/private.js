4c12708e4a4d84cf3bb0fcb10bbfe38fe38797e8
Rahul Diwakar <rahuldiwakar611@gmail.com>
2021-09-30 10:16:36 +0530

    image uploaded

--- routes/private.js
+++ routes/private.js
@@ -3,15 +3,24 @@ const router = express.Router();
 const { getPrivateData} = require("../controller/private.js");
 const { protect } = require("../middleware/auth.js");
 const upload = require("../utils/multer");
+const User = require("../model/user")
 
 router.route("/").get(protect, getPrivateData);
 router.route("/upload-avatar").patch(
     protect,
     upload.single("avatar"),
-    function(req, res, next) {
-        // Update user avatar here with path (http://localhost:5000/${req.file.path})
-        
-        res.send("Image uploaded successful");
+    async function(req, res, next) {
+        try {
+            // Update user avatar here with path (http://localhost:5000/${req.file.path})
+            const user = await User.findOne({email:req.user.email})
+            
+            user.avatar = `http://localhost:5000/${req.file.path}`
+            user.save();
+
+            res.send("Image uploaded successful");
+        } catch (error) {
+            next(error)
+        }
     }
 );
 
