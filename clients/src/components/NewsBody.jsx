6e41907dc10c15629818118c0ced0a4c57602f96
Rahul Diwakar <rahuldiwakar611@gmail.com>
2021-10-05 13:46:19 +0530

    newsbody section created in client

--- clients/src/components/NewsBody.jsx
+++ clients/src/components/NewsBody.jsx
@@ -3,7 +3,38 @@ import "../stylesheet/newsbody.css"
 function NewsBody(){
     return(
         <div className="newsbody">
-            <section className=""></section>
+            <section className="newsbody-header">
+                <h1>Local Morning News</h1>  
+            </section>
+            <hr />
+            <section>
+                <ul>
+                    <li>
+                        <div>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non mai labore consequuntur nostrum neque f</p>
+                            <small> 1hr ago</small>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non mai labore consequuntur nostrum neque f</p>
+                            <small> 1hr ago</small>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non mai labore consequuntur nostrum neque f</p>
+                            <small> 1hr ago</small>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non mai labore consequuntur nostrum neque f</p>
+                            <small> 1hr ago</small>
+                        </div>
+                    </li>
+                </ul>
+            </section>
         </div>
     )
 }
--- clients/src/components/PostForm.jsx
+++ clients/src/components/PostForm.jsx
@@ -33,6 +33,9 @@ function PostForm(){
                 <div>
                     <img className="postimg" src="https://www.thesprucepets.com/thmb/kV_cfc9P4QWe-klxZ8y--awxvY4=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/adorable-white-pomeranian-puppy-spitz-921029690-5c8be25d46e0fb000172effe.jpg" alt="dog" />
                 </div>
+                <div>
+                    <button>Like</button>
+                </div>
             </section>
             <section className="postform-post">
                 <div className="form-post-header">
@@ -46,7 +49,11 @@ function PostForm(){
                 <div>
                     <img className="postimg" src="https://www.thesprucepets.com/thmb/kV_cfc9P4QWe-klxZ8y--awxvY4=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/adorable-white-pomeranian-puppy-spitz-921029690-5c8be25d46e0fb000172effe.jpg" alt="dog" />
                 </div>
+                <div>
+                    <button>Like</button>
+                </div>
             </section>
+            
         </div>
     )
 }
